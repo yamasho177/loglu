5addcc0c5aa60d0e8e8b98a1294b02d353574b19
yamasho177 <yamasho177@gmail.com>
2026-05-05 17:08:55 +0900

    Deviceの導入と初期設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+ 
+  protected
+ 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
