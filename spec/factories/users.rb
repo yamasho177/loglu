2802a8591375f06972f325e195b4cb4944fa2de2
yamasho177 <yamasho177@gmail.com>
2026-05-06 09:58:39 +0900

    User.nicknameの文字数の関するバリデーションを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname { 'サンプル太郎' }
+    sequence(:email) do |n|
+      "test#{n}@example.com"
+    end
+    password { '1111111' }
+    password_confirmation { '1111111' }
   end
 end
