9528be1c7be34a0f70436578c1a9fe4c32c8dbd7
yamasho177 <yamasho177@gmail.com>
2026-05-07 00:00:47 +0900

    Postモデルを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_05_080107) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_06_143405) do
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -23,4 +32,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_05_080107) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
